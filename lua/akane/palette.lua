ae8e77998af979e81e6b9f2215f71e30a8a620a6
M1nts02 <m1nts02@M1nts02s-Air.lan>
2023-11-27 11:42:17 +0800

    fix bug

---
+++ lua/akane/palette.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+M.light = {
+  bg = "#fafafa",
+  fg = "#575f66",
+
+  line = "#eff0f9",
+  line_nr = "#55b4d4",
+  cursor_line_nr = "#1e90ff",
+
+  white = "#eff0f9",
+  black = "#1E1D2D",
+
+  cyan = "#008b8b",
+  blue = "#399ee6",
+  green = "#69ac00",
+  light_green = "#b6f4be",
+  orange = "#ff7f24",
+  yellow = "#ffa54f",
+  grey = "#9c9c9c",
+  purple = "#bf3eff",
+  dark_purple = "#a360cc",
+  indian_red = "#ff6a6a",
+  red = "#f51818",
+  pink = "#ff34b3",
+  light_pink = "#ffa5c3",
+}
+
+M.dark = {
+  bg = "#2b2d3a",
+  fg = "#D9E0EE",
+
+  line = "#333648",
+  line_nr = "#55b4d4",
+  cursor_line_nr = "#1e90ff",
+
+  white = "#D9E0EE",
+  black = "#1E1D2D",
+
+  cyan = "#00ffff",
+  blue = "#87cefa",
+  green = "#a9dc76",
+  light_green = "#b6f4be",
+  orange = "#ef9062",
+  yellow = "#e5c463",
+  grey = "#9c9c9c",
+  purple = "#ab9df2",
+  dark_purple = "#a360cc",
+  indian_red = "#ff6a6a",
+  red = "#f51818",
+  pink = "#ff69b4",
+  light_pink = "#F5C2E7",
+}
+
+return M
--- lua/lualine/themes/akane.lua
+++ lua/lualine/themes/akane.lua
@@ -1,4 +1,4 @@
-local palette = require("akane.paleete")["light"]
+local palette = require("akane.palette")["light"]
 palette.bg = vim.o.background == "light" and "#fafafa" or "#2b2d3a"
 
 return {

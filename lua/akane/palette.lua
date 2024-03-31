500e246277218a9943ce14e013dfa19cc1d8ea1a
M1nts02 <zyh1032712227@outlook.com>
2024-03-31 19:58:20 +0800

    Update color

--- lua/akane/palette.lua
+++ lua/akane/palette.lua
@@ -2,54 +2,44 @@ local M = {}
 
 M.light = {
   bg = "#fafafa",
-  fg = "#575f66",
+  bg1 = "#e6e9ef",
+  bg2 = "#dce0e8",
 
-  line = "#eff0f9",
-  line_nr = "#55b4d4",
-  cursor_line_nr = "#1e90ff",
-
-  white = "#eff0f9",
-  black = "#1E1D2D",
+  fg = "#4c4f69",
+  fg1 = "#8c8fa1",
+  fg2 = "#9ca0b0",
+  fg3 = "#ccd0da",
 
   cyan = "#008b8b",
   blue = "#399ee6",
   green = "#69ac00",
-  light_green = "#b6f4be",
   orange = "#ff7f24",
   yellow = "#ffa54f",
-  grey = "#9c9c9c",
   purple = "#bf3eff",
-  dark_purple = "#a360cc",
   indian_red = "#ff6a6a",
   red = "#f51818",
   pink = "#ff34b3",
-  light_pink = "#ffa5c3",
 }
 
 M.dark = {
   bg = "#1E1E2E",
+  bg1 = "#303446",
+  bg2 = "#45475a",
+
   fg = "#D9E0EE",
+  fg1 = "#bac2de",
+  fg2 = "#7f849c",
+  fg3 = "#585b70",
 
-  line = "#333648",
-  line_nr = "#55b4d4",
-  cursor_line_nr = "#1e90ff",
-
-  white = "#D9E0EE",
-  black = "#1E1D2D",
-
-  cyan = "#00ffff",
+  cyan = "#008b8b",
   blue = "#87cefa",
   green = "#a9dc76",
-  light_green = "#b6f4be",
   orange = "#ef9062",
   yellow = "#e5c463",
-  grey = "#9c9c9c",
   purple = "#ab9df2",
-  dark_purple = "#a360cc",
   indian_red = "#ff6a6a",
   red = "#f51818",
   pink = "#ff69b4",
-  light_pink = "#F5C2E7",
 }
 
 return M
